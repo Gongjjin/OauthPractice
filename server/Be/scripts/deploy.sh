aa47cfb35fcdbff7ca1238826ec04f226d4b82d3
Gongjjin <gjyj5395@naver.com>
2025-04-02 17:24:09 +0900

    deploy.sh의 위치를 scripts안으로 변경

--- server/Be/scripts/deploy.sh
+++ server/Be/scripts/deploy.sh
@@ -6,8 +6,6 @@ cd /home/ubuntu/app
 # 환경변수 DOCKER_APP_NAME을 spring으로 설정
 DOCKER_APP_NAME=spring
 
-# 주석 추가
-
 # 실행중인 blue가 있는지 확인
 # 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지 여부를 EXIST_BLUE 변수에 저장
 EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
